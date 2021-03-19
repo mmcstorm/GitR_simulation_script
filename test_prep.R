9f4359c0fc2620c26d9faabcad2fafcc3fbb78b7
Maxine Storm <MaxineStorm@MacBook-Pro.local>
2021-03-19 10:15:57 +0100

    change I will later undo

--- test_prep.R
+++ test_prep.R
@@ -3,3 +3,10 @@ factors <- c(2,4)
 nobs <- c(50,100)
 ncat <- c(2,4)
 Design_try <- expand.grid(factors = factors, nobs = nobs, ncat = ncat)
+
+n.replications <- 2
+factors <- 2
+nobs <- 50
+ncat <- 2
+nvarp <- 6
+nvar <- 12
